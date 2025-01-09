4e8620b449c91bfc7d0319958abed788cbfa70fa
Anubhav <anubhav.paraashar@gmail.com>
2025-01-09 13:24:02 +0530

    second commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,28 +2,54 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-[#0C0722]  p-10">
-      <div className="max-w-screen-xl mx-auto px-4 md:px-10 lg:px-20 flex flex-col md:flex-row justify-between items-center  my-20">
-        {/* Left Side: TradyLytics */}
-        <h1 className="text-4xl md:text-6xl font-semibold text-zinc-50 leading-none tracking-tight mb-6 md:mb-0">
-          TradyLytics
-        </h1>
-
-        {/* Right Side: Social Links */}
-        <div className="flex gap-6">
-          {[
-            { name: "Instagram", link: "#instagram" },
-            { name: "Twitter", link: "#twitter" },
-            { name: "LinkedIn", link: "#linkedin" },
-          ].map((social, index) => (
+    <div className="w-full bg-[#0C0722]">
+      <div className="max-w-screen-xl mx-auto py-10 px-6 lg:px-0 flex flex-col lg:flex-row gap-12 lg:gap-32 text-zinc-50 mt-44">
+        <div className="lg:basis-1/2">
+          <h1 className="text-4xl md:text-6xl lg:text-[8rem] font-semibold leading-none tracking-tight">
+            TradyLytics
+          </h1>
+        </div>
+        <div className="lg:basis-1/2 flex flex-col sm:flex-row gap-8">
+          <div className="basis-1/3">
+            <h4 className="mb-6 text-zinc-500 text-sm md:text-base">Socials</h4>
+            {["Instagram", "Twitter", "LinkedIn"].map((item, index) => (
+              <a
+                key={index}
+                href={`#${item}`}
+                className="block mt-2 text-zinc-600 text-sm md:text-base hover:text-zinc-300"
+              >
+                {item}
+              </a>
+            ))}
+          </div>
+          <div className="basis-1/3">
+            <h4 className="mb-6 text-zinc-500 text-sm md:text-base">
+              Contact Us
+            </h4>
             <a
-              key={index}
-              href={social.link}
-              className="text-zinc-300 hover:text-zinc-100 text-lg"
+              href="mailto:info@tradylytics.in"
+              className="block mt-2 text-zinc-600 text-sm md:text-base hover:text-zinc-300"
             >
-              {social.name}
+              info@tradylytics.in
             </a>
-          ))}
+            <a
+              href="tel:+91 9876543210"
+              className="block mt-2 text-zinc-600 text-sm md:text-base hover:text-zinc-300"
+            >
+              +91 9166452134
+            </a>
+          </div>
+
+          <div className="basis-1/2 flex flex-col items-start lg:items-end rounded-md">
+            <h4 className="mb-4 text-zinc-50 text-lg font-semibold">
+              TradyLytics
+            </h4>
+            <p className="pb-2 text-left lg:text-right text-sm md:text-base">
+              318 Sector 5 Avas Vikas Colony, Sikandra, Agra,
+              <br /> Uttar Pradesh, 282007 <br />
+              India
+            </p>
+          </div>
         </div>
       </div>
     </div>
